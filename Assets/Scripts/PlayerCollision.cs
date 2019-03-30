9104a03adc672297311589fc39717ffd11cabccb
Quentin Désert <qtdesert@gmail.com>
2019-03-29 19:28:10 -0700

    E08 - Game Over
    What happens when we lose?

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,11 +2,14 @@
 
 public class PlayerCollision : MonoBehaviour {
 
-    public PlayerMovement movement;
+    public PlayerMovement movement; // A reference to our PlayerMovement script
 
+    // This function runs when we hit another object.
+    // We get information about the collision and call it "collisionInfo".
     private void OnCollisionEnter(Collision collisionInfo) {
         if (collisionInfo.collider.tag == "Obstacle") {
-            movement.enabled = false;
+            movement.enabled = false; // Disable the players movement
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,11 @@
 
 public class PlayerMovement : MonoBehaviour {
 
+    // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float forwardForce = 2000f; // Variable that determines the forward force
+    public float sidewaysForce = 500f; // Variable that determines the sideways force
 
     // Update is called once per frame (FixedUpdate because we are using it to mess with physics)
     void FixedUpdate() {
@@ -13,11 +14,17 @@ public class PlayerMovement : MonoBehaviour {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d")) {
+            // Add a force to the right
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
+            // Add a force to the left
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
